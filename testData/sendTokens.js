1937aedfa68c0082ad6c4dad23d0fdfa70b1de7b
Anuj <ashah812@gmail.com>
2018-11-16 13:48:03 -0800

    updating data scripts

---
+++ testData/sendTokens.js
@@ -0,0 +1,24 @@
+const {getAccounts} = require('./utils');
+
+const sendTokens = async (utils) => {
+  const accounts = await getAccounts();
+  const seedAmount = 10 ** 20;
+  const {token} = utils;
+  console.log('checking token balances');
+  const tokenPromises = accounts.map(async (account,i) => {
+   	if(i>0){
+      const balance = await token.balanceOf(accounts[i]);
+      if(balance.c[0] === 0){
+        try{
+          console.log(`sending LIN to ${accounts[i]}`);
+  	    return token.transfer(accounts[i].toLowerCase(), seedAmount, {from: accounts[0].toLowerCase(), gas: 500000 });
+        }
+        catch(e){console.log(e);}
+      }
+    }
+  });
+  await Promise.all(tokenPromises);
+  console.log('all accounts have LIN!');
+};
+
+module.exports = {sendTokens};
--- testData/setupOffers.js
+++ testData/setupOffers.js
@@ -4,15 +4,19 @@ const {getAccounts} = require('./utils');
 const setupOffers = async (utils) => {
   const accounts = await getAccounts();
   const {offers, events, token } = utils;
+  const offerPromises = [];
+  console.log('making offers');
   for(let i=0; i<4; i++){
     try{
       const hashes = events.slice(i*10,(i*10)+10).map(event => event.dataHash);
-      await token.approve(offers.address, 10, { from: accounts[i+1], gas: 500000, gasPrice: 1});
-      await offers.makeOffers(hashes, 'HQmiMPrZAcfg3gQYNaFoEJVZbKCNA3NCazHAiXE+Yjg=', [1,1,1,1,1,1,1,1,1,1], {from: accounts[i+1], gas: 5000000, gasPrice: 1});
-      console.log(`offers made by ${accounts[i+1]} for dataHashes: ${hashes.join(', ')}`);
+      offerPromises.push(token.approve(offers.address, 10, { from: accounts[i+1].toLowerCase(), gas: 500000}).then(() => {
+        offers.makeOffers(hashes, 'HQmiMPrZAcfg3gQYNaFoEJVZbKCNA3NCazHAiXE+Yjg=', [1,1,1,1,1,1,1,1,1,1], {from: accounts[i+1].toLowerCase(), gas: 5000000});
+        }));
     }
     catch(e){console.log(e);}
   }
+  await Promise.all(offerPromises);
+  console.log('all offers made!');
 };
 
 module.exports={setupOffers};
