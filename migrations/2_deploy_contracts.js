f6105ef4d359022024abc7fec89b26e0af80bd65
MaxBlaushild <blaushmild@gmail.com>
2018-11-26 23:19:01 -0500

    makes it easier to deploy contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,14 +2,21 @@ const StowOffers = artifacts.require('./StowOffers.sol');
 const StowStaking = artifacts.require('./StowStaking.sol');
 const StowDDEXHub = artifacts.require('./StowDDEXHub.sol');
 
-const { ropsten } = require('@stowprotocol/stow-addresses');
-const { StowSmartContracts, StowToken } = ropsten;
+const addresses = require('@stowprotocol/stow-addresses');
 
-const hubAddress = '' || StowSmartContracts.latest;
-const tokenAddress = '' || StowToken.latest;
+const getContractAddresses = network => {
+  const contracts = addresses[network];
+
+  return {
+    hubAddress: contracts ? contracts.StowSmartContracts.latest : '',
+    tokenAddress: contracts ? contracts.StowToken.latest : '',
+  };
+};
 
 module.exports = (deployer, network, accounts) => {
 	let ddexHubInstance;
+
+  const { hubAddress, tokenAddress } = getContractAddresses(network);
 	// deploy ddexHub
 	return deployer.deploy(StowDDEXHub, hubAddress, tokenAddress)
 	 .then((_ddexHubInstance) => {
